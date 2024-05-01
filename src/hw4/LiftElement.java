7c094b75516eb79030352ecf527d90235a83ae3f
ArjavTripathi <arjavtri@iastate.edu>
2024-05-01 15:53:17 -0500

    skeleton done

--- src/hw4/LiftElement.java
+++ src/hw4/LiftElement.java
@@ -1,5 +1,9 @@
 package hw4;
 
+import api.AbstractElement;
+
+import java.util.ArrayList;
+
 /**
  * An element with two distinctive behaviors. First, it can be set up to move
  * vertically within a fixed set of boundaries. On reaching a boundary, the
@@ -11,6 +15,13 @@ package hw4;
  */
 //TODO: This class must directly or indirectly extend AbstractElement
 public class LiftElement extends CommonMethods {
+
+	private ArrayList<AbstractElement> elements;
+
+	private double deltaX;
+	private double deltaY;
+	private boolean flag;
+
 
 	/**
 	 * Constructs a new Elevator. Initially the upper and lower boundaries are
@@ -24,11 +35,44 @@ public class LiftElement extends CommonMethods {
 	 */
 	public LiftElement(double x, double y, int width, int height) {
 		super(x, y, width, height);
+		this.elements = new ArrayList<>();
+		this.flag = false;
+		this.deltaX = 0;
+		this.deltaY = 0;
 	}
 
 	@Override
 	public void update(){
+		super.update();
+	}
 
+	public void setVelocity(double deltaX, double deltaY){
+		this.deltaX = deltaX;
+		this.deltaY = deltaY;
+	}
+	public void setBounds(double min, double max){
+		setMin(min);
+		setMax(max);
+	}
+
+	public java.util.ArrayList<AbstractElement> getAssociated(){
+		return elements;
+	}
+
+	public void addAssociated(FollowerElement follower){
+		elements.add(follower);
+	}
+
+	public void addAssociated(AttachedElement attached){
+		attached.setBase(this);
+		elements.add(attached);
+	}
+	public void deleteMarkedAssociated(){
+		for(AbstractElement e : this.elements){
+			if(e.isMarked()){
+				elements.remove(e);
+			}
+		}
 	}
 
 }
